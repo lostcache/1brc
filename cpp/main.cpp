93ceac514d06d8187a5a53caca5c8cdc7cbdf1c9
lostcache <harshdevisha9111@gmail.com>
2025-12-31 01:13:24 +0000

    Most likely the final solution

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,9 +8,9 @@
 #include <filesystem>
 #include <iostream>
 #include <limits>
-#include <numeric>
 #include <string>
 #include <string_view>
+#include <sys/cdefs.h>
 #include <sys/fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -18,137 +18,127 @@
 #include <unistd.h>
 #include <vector>
 
-constexpr size_t NUM_THREADS = 16;
+constexpr size_t NUM_THREADS = 8;
 
 class FastMap {
   private:
-    std::vector<int32_t> minVec_;
-    std::vector<int32_t> maxVec_;
-    std::vector<size_t> freqVec_;
-    std::vector<int64_t> sumVec_;
-    std::vector<std::string> locationVec_;
+    struct LocationEntry_ {
+        int32_t min;
+        int32_t max;
+        size_t freq;
+        int64_t sum;
+        std::string location;
+        LocationEntry_() {
+            min = std::numeric_limits<int32_t>::max();
+            max = std::numeric_limits<int32_t>::min();
+            freq = 0;
+            sum = 0;
+        }
+    };
+    std::vector<LocationEntry_> data;
     size_t capacity_;
     size_t mask_;
 
     size_t fast_hash(std::string_view sv) {
-        size_t hash = 14695981039346656037ULL;
-        for (char c : sv) {
-            hash ^= static_cast<size_t>(c);
-            hash *= 1099511628211ULL;
-        }
+        assert(!sv.empty());
+
+        uint64_t hash = 0;
+        for (size_t index = 0; index < sv.size(); ++index)
+            hash = hash * 1315423911u + static_cast<unsigned char>(sv[index]);
         return hash;
     }
 
-    std::string_view getLocation(size_t idx) const { return this->locationVec_[idx]; }
+    size_t getIdx(std::string_view queryLocation) {
+        assert(!queryLocation.empty());
 
-    int32_t getMin(size_t idx) const { return this->minVec_[idx]; }
-
-    int32_t getMax(size_t idx) const { return this->maxVec_[idx]; }
-
-    int64_t getSum(size_t idx) const { return this->sumVec_[idx]; }
-
-    size_t getFreq(size_t idx) const { return this->freqVec_[idx]; }
-
-    size_t getIdx(std::string_view location) {
-        size_t hash = fast_hash(location);
-        size_t idx = hash & mask_;
+        size_t hash = fast_hash(queryLocation);
+        size_t idx = hash & this->mask_;
         while (true) {
-            const auto& locationEntry = this->locationVec_[idx];
-            if (locationEntry.size() <= 0 || locationEntry == location) {
+            const auto& location = this->data[idx].location;
+            if (location.size() <= 0 || location == queryLocation) {
                 return idx;
             }
             idx = (idx + 1) & mask_;
         }
     }
 
-    void updateLocationEntry(size_t idx, std::string_view location) {
-        const auto& locationEntry = this->locationVec_[idx];
-        if (locationEntry.size() <= 0) {
-            this->locationVec_[idx] = std::string(location);
-        }
-    }
-
-    void updateMin(size_t idx, int32_t temperature) {
-        this->minVec_[idx] = std::min(this->minVec_[idx], temperature);
-    }
-
-    void updateMax(size_t idx, int32_t temperature) {
-        this->maxVec_[idx] = std::max(this->maxVec_[idx], temperature);
-    }
-
-    void updateSum(size_t idx, int64_t value) { this->sumVec_[idx] += value; }
-
-    void updateFreq(size_t idx) { this->freqVec_[idx]++; }
-
-    void updateFreq(size_t idx, size_t value) { this->freqVec_[idx] += value; }
-
-    std::vector<size_t> sortKeyIndices() const {
-        std::vector<size_t> indices(this->size());
-        std::iota(indices.begin(), indices.end(), 0);
-
-        std::sort(indices.begin(), indices.end(), [&](const auto& a, const auto& b) {
-            return this->locationVec_[a] < this->locationVec_[b];
-        });
-
-        return indices;
-    }
-
     double roundTowardsINF(double value) const { return std::round(value * 10.0) / 10.0; }
+
+    bool sortInplace() {
+        assert(this->data.size() > 0);
+
+        std::sort(this->data.begin(), this->data.end(),
+                  [](const LocationEntry_& a, const LocationEntry_& b) {
+                      return a.location < b.location;
+                  });
+
+        return true;
+    }
 
   public:
     FastMap(size_t initCap = 1 << 14) : capacity_(initCap), mask_(initCap - 1) {
-        minVec_.resize(initCap, std::numeric_limits<int32_t>::max());
-        maxVec_.resize(initCap, std::numeric_limits<int32_t>::min());
-        freqVec_.resize(initCap, 0);
-        sumVec_.resize(initCap, 0);
-        locationVec_.resize(initCap);
+        this->data.resize(initCap, LocationEntry_());
     }
 
     size_t size() const { return this->capacity_; }
 
-    void updateRunning(std::string_view location, int32_t temperature) {
+    __attribute__((hot)) bool update(std::string_view location, int32_t temperature) {
+        assert(!location.empty());
+
         size_t idx = this->getIdx(location);
-        this->updateLocationEntry(idx, location);
-        this->updateMin(idx, temperature);
-        this->updateMax(idx, temperature);
-        this->updateFreq(idx);
-        this->updateSum(idx, temperature);
+
+        assert(idx >= 0 && idx < this->capacity_);
+
+        if (this->data[idx].location.size() <= 0) {
+            this->data[idx].location = std::string(location);
+        }
+        this->data[idx].freq += 1;
+        this->data[idx].sum += temperature;
+        this->data[idx].min = std::min(this->data[idx].min, temperature);
+        this->data[idx].max = std::max(this->data[idx].max, temperature);
+
+        return true;
     }
 
-    void updateBatch(const FastMap& other) {
-        for (size_t otherIdx = 0; otherIdx < other.size(); ++otherIdx) {
-            const auto& otherLocation = other.getLocation(otherIdx);
+    bool update(const FastMap& other) {
+        for (size_t i = 0; i < other.size(); ++i) {
+            const auto& otherLocation = other.data[i].location;
+
             if (otherLocation.size() <= 0) continue;
+
             size_t thisIdx = this->getIdx(otherLocation);
 
-            this->updateLocationEntry(thisIdx, otherLocation);
-            this->updateMin(thisIdx, other.getMin(otherIdx));
-            this->updateMax(thisIdx, other.getMax(otherIdx));
-            this->updateFreq(thisIdx, other.getFreq(otherIdx));
-            this->updateSum(thisIdx, other.getSum(otherIdx));
+            if (this->data[thisIdx].location.size() <= 0) {
+                this->data[thisIdx].location = otherLocation;
+            }
+            this->data[thisIdx].freq += other.data[i].freq;
+            this->data[thisIdx].sum += other.data[i].sum;
+            this->data[thisIdx].min = std::min(this->data[thisIdx].min, other.data[i].min);
+            this->data[thisIdx].max = std::max(this->data[thisIdx].max, other.data[i].max);
         }
+
+        return true;
     }
 
-    void printSorted() const {
-        std::vector<size_t> sortedKeyIndices = this->sortKeyIndices();
+    void printSorted() {
+        this->sortInplace();
 
         std::string outBuffer;
         outBuffer.reserve(2 * 1024 * 1024);
 
-        for (auto idx : sortedKeyIndices) {
-            if (outBuffer.size() > 1) outBuffer += ", ";
-
-            const auto& location = this->getLocation(idx);
+        for (const auto& locEntry : this->data) {
+            const auto& location = locEntry.location;
 
             if (location.size() <= 0) continue;
+
+            if (outBuffer.size() > 1) outBuffer += ", ";
 
             outBuffer += location;
             outBuffer += "=";
 
-            double min_val = this->getMin(idx) / 10.0;
-            double max_val = this->getMax(idx) / 10.0;
-            double avg =
-                roundTowardsINF(this->getSum(idx) / static_cast<double>(this->getFreq(idx) * 10));
+            double min_val = locEntry.min / 10.0;
+            double max_val = locEntry.max / 10.0;
+            double avg = roundTowardsINF(locEntry.sum / static_cast<double>(locEntry.freq * 10));
 
             size_t pos = outBuffer.size();
             outBuffer.resize(pos + 32);
@@ -195,23 +185,27 @@ void unMapFile(MMAPFile f) { munmap(const_cast<char*>(f.filePtr), f.fileSize); }
 size_t getBatchSize(size_t fileSize) { return (fileSize + (NUM_THREADS - 1)) / NUM_THREADS; }
 
 // Assumes format: [-]D[D].D where D is digit
-int32_t parseInt32(const char* start, const char* end) {
-    bool neg = (*start == '-');
-    if (neg) start++;
-    size_t len = end - start;
-    int32_t num = 0;
-    if (len == 3) {
-        num += (*(start + 0) & 0xF) * 10;
-        num += (*(start + 2) & 0xF) * 1;
-    } else if (len == 4) {
-        num += (*(start + 0) & 0xF) * 100;
-        num += (*(start + 1) & 0xF) * 10;
-        num += (*(start + 3) & 0xF) * 1;
+int32_t parseInt32(const char* start) {
+    int32_t value = 0;
+    bool isNegative = false;
+
+    if (start[0] == '-') {
+        isNegative = true;
+        start++;
     }
-    return neg ? -num : num;
+
+    value = (start[0] - '0') * 10 + start[2] - '0';
+
+    if (start[2] == '.') {
+        value = (start[0] - '0') * 100 + (start[1] - '0') * 10 + start[3] - '0';
+    }
+
+    return isNegative ? -value : value;
 }
 
 std::pair<std::string_view, int32_t> parseLine(std::string_view line) {
+    assert(!line.empty());
+
     size_t semicolonPos = line.find(';');
     assert(semicolonPos != std::string_view::npos);
     std::string_view locationView = line.substr(0, semicolonPos);
@@ -230,15 +224,13 @@ std::pair<std::string_view, int32_t> parseLine(std::string_view line) {
     assert(tempEnd > tempStart);
 
     const char* start = line.data() + tempStart;
-    const char* end = line.data() + tempEnd;
-    int32_t temp_int = parseInt32(start, end);
+    int32_t temp_int = parseInt32(start);
 
     return std::make_pair(locationView, temp_int);
 }
 
-size_t skipTillNextLine(size_t threadIndex, size_t startPos, MMAPFile f) {
+size_t skipTillNextLine(size_t startPos, MMAPFile f) {
     assert(startPos > 0);
-    assert(threadIndex > 0);
 
     const char* prevChar = f.filePtr + startPos - 1;
     if (*prevChar == '\n') {
@@ -257,10 +249,10 @@ size_t skipTillNextLine(size_t threadIndex, size_t startPos, MMAPFile f) {
     return bytesSkipped;
 }
 
-void processLinesInCurrBatch(size_t startPos, size_t batchEnd, MMAPFile f, FastMap& m) {
+void processLinesInCurrBatch(size_t startPos, size_t batchEndPos, MMAPFile f, FastMap& m) {
     size_t pos = startPos;
 
-    while (pos < batchEnd && pos < f.fileSize) {
+    while (pos < batchEndPos && pos < f.fileSize) {
         size_t newlinePos = pos;
         while (newlinePos < f.fileSize && *(f.filePtr + newlinePos) != '\n') {
             ++newlinePos;
@@ -272,8 +264,7 @@ void processLinesInCurrBatch(size_t startPos, size_t batchEnd, MMAPFile f, FastM
 
         auto result = parseLine(line);
         auto [location, temperature] = result;
-        m.updateRunning(location, temperature);
-        assert(m.size() > 0);
+        m.update(location, temperature);
 
         pos = newlinePos + 1;
     }
@@ -287,7 +278,7 @@ void accumulateBatch(size_t threadIndex, size_t startPos, size_t batchSizeBytes,
 
     // Skip to the next line boundary at the start for non-zero threads
     if (threadIndex > 0) {
-        size_t bytesSkipped = skipTillNextLine(threadIndex, startPos, f);
+        size_t bytesSkipped = skipTillNextLine(startPos, f);
         startPos += bytesSkipped;
     }
 
@@ -296,7 +287,7 @@ void accumulateBatch(size_t threadIndex, size_t startPos, size_t batchSizeBytes,
 
 void accumulateThreadResults(const std::vector<FastMap>& maps, FastMap& finalMap) {
     for (const auto& m : maps) {
-        finalMap.updateBatch(m);
+        finalMap.update(m);
     }
 }
 
@@ -317,7 +308,7 @@ void processInBatches(MMAPFile f, size_t batchSize, FastMap& finalMap) {
     accumulateThreadResults(maps, finalMap);
 }
 
-void processInOneBatch(MMAPFile f, FastMap& finalMap) {
+void processLinesInSingleBatch(MMAPFile f, FastMap& finalMap) {
     accumulateBatch(0, 0, f.fileSize, finalMap, f);
 }
 
@@ -329,7 +320,7 @@ void accumulate(MMAPFile f, FastMap& finalMap) {
     if (batchSize > 4 * 1024) {
         processInBatches(f, batchSize, finalMap);
     } else {
-        processInOneBatch(f, finalMap);
+        processLinesInSingleBatch(f, finalMap);
     }
 }
 
